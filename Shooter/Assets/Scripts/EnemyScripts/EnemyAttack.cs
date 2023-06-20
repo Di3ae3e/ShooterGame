e6c0a28444a8c865b1971542d0c797482ad2d0c3
OLEVANDR55 <83229879+OLEVANDR55@users.noreply.github.com>
2023-06-20 22:32:51 +0300

    Update AI for enemy

--- Shooter/Assets/Scripts/EnemyScripts/EnemyAttack.cs
+++ Shooter/Assets/Scripts/EnemyScripts/EnemyAttack.cs
@@ -4,8 +4,37 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
+    public float attackRotateSpeed;
     public float speedAttack;
     public Transform target;
 
-    
+    public EnemyPatrol patrolScript;
+    public Transform enemyPosition;
+
+    private void Update()
+    {
+        RotateToPlayer();
+    }
+
+    public void RotateToPlayer()
+    {
+        if(patrolScript.enabled == false)
+        {
+            Vector2 movementDiretcion = Vector2.MoveTowards(enemyPosition.position, target.position, speedAttack * Time.deltaTime);
+            enemyPosition.position = movementDiretcion;
+
+            Vector3 targetDir = target.position - enemyPosition.position;
+            float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg - 90;
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            enemyPosition.rotation = Quaternion.Slerp(enemyPosition.rotation, q, attackRotateSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            patrolScript.enabled = false;
+        }
+    }
 }
--- Shooter/Assets/Scripts/EnemyScripts/EnemyPatrol.cs
+++ Shooter/Assets/Scripts/EnemyScripts/EnemyPatrol.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class EnemyPatrol : MonoBehaviour
 {
     public float speedPatrol;
+    public float rotateSpeed;
+
     private float waitTime;
     public float startWaitTime;
 
@@ -19,9 +22,21 @@ public class EnemyPatrol : MonoBehaviour
 
     private void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, moveSpot[randomSpot].position, speedPatrol * Time.deltaTime);
-        if(Vector2.Distance(transform.position, moveSpot[randomSpot].position) < 0.2f)
+        Vector2 movementDiretcion = Vector2.MoveTowards(transform.position, moveSpot[randomSpot].position, speedPatrol * Time.deltaTime);
+        transform.position = movementDiretcion;
+
+        //Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, movementDiretcion);
+        //transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotateSpeed * Time.deltaTime);
+
+        Vector3 targetDir = moveSpot[randomSpot].position - transform.position;
+        float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg - 90;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, rotateSpeed * Time.deltaTime);
+
+
+        if (Vector2.Distance(transform.position, moveSpot[randomSpot].position) < 0.2f)
         {
+            
             if (waitTime <= 0)
             {
                 randomSpot = Random.Range(0, moveSpot.Length);
